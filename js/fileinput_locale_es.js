171fbe449f1d33b26f7c800ade45c92e3884ed16
rapomon <rapomon@gmail.com>
2015-07-01 11:41:03 +0200

    Update fileinput_locale_es.js
    Minor change.

--- js/fileinput_locale_es.js
+++ js/fileinput_locale_es.js
@@ -1,5 +1,5 @@
 /*!
- * FileInput Spanish (Latin American) Translations
+ * FileInput Spanish Translations
  *
  * This file must be loaded after 'fileinput.js'. Patterns in braces '{}', or
  * any HTML markup tags in the messages must not be converted or translated.
@@ -14,32 +14,32 @@
     $.fn.fileinputLocales['es'] = {
         fileSingle: 'archivo',
         filePlural: 'archivos',
-        browseLabel: 'Buscar &hellip;',
-        removeLabel: 'Remover',
-        removeTitle: 'Limpiar archivos seleccionados',
+        browseLabel: 'Examinar &hellip;',
+        removeLabel: 'Quitar',
+        removeTitle: 'Quitar archivos seleccionados',
         cancelLabel: 'Cancelar',
-        cancelTitle: 'Abortar el cargue en curso',
-        uploadLabel: 'Cargar Archivo',
-        uploadTitle: 'Cargar archivos seleccionados',
-        msgSizeTooLarge: 'Archivo "{name}" (<b>{size} KB</b>) excede el tamaño máximo permitido de <b>{maxSize} KB</b>. Por favor reintente su cargue!',
-        msgFilesTooLess: 'Usted debe seleccionar al menos <b>{n}</b> {files} a cargar. Por favor reintente su cargue!',
-        msgFilesTooMany: 'El número de archivos seleccionados a cargar <b>({n})</b> excede el límite máximo permitido de <b>{m}</b>. Por favor reintente su cargue!',
-        msgFileNotFound: 'Archivo "{name}" no encontrado!',
-        msgFileSecured: 'Restricciones de seguridad previenen la lectura del archivo "{name}".',
-        msgFileNotReadable: 'Archivo "{name}" no se puede leer.',
-        msgFilePreviewAborted: 'Previsualización del archivo abortada para "{name}".',
+        cancelTitle: 'Abortar la subida en curso',
+        uploadLabel: 'Subir archivo',
+        uploadTitle: 'Subir archivos seleccionados',
+        msgSizeTooLarge: 'Archivo "{name}" (<b>{size} KB</b>) excede el tamaño máximo permitido de <b>{maxSize} KB</b>. Por favor, inténtelo de nuevo con archivos mas pequeños.',
+        msgFilesTooLess: 'Debe seleccionar al menos <b>{n}</b> {files} a cargar. Por favor, inténtelo de nuevo.',
+        msgFilesTooMany: 'El número de archivos seleccionados a cargar <b>({n})</b> excede el límite máximo permitido de <b>{m}</b>. Por favor, inténtelo de nuevo.',
+        msgFileNotFound: 'Archivo "{name}" no encontrado.',
+        msgFileSecured: 'No es posible acceder al archivo "{name}" porque estará siendo usado por otra aplicación o no tengamos permisos de lectura.',
+        msgFileNotReadable: 'No es posible acceder al archivo "{name}".',
+        msgFilePreviewAborted: 'Previsualización del archivo "{name}" cancelada.',
         msgFilePreviewError: 'Ocurrió un error mientras se leía el archivo "{name}".',
-        msgInvalidFileType: 'Tipo de archivo inválido para el archivo "{name}". Sólo archivos "{types}" son permitidos.',
-        msgInvalidFileExtension: 'Extensión de archivo inválido para "{name}". Sólo archivos "{extensions}" son permitidos.',
-        msgValidationError: 'Error Cargando Archivo',
-        msgLoading: 'Cargando archivo {index} of {files} &hellip;',
-        msgProgress: 'Cargando archivo {index} of {files} - {name} - {percent}% completado.',
-        msgSelected: '{n} {files} seleccionados',
-        msgFoldersNotAllowed: 'Arrastre y suelte únicamente archivos! Se omite {n} carpeta(s).',
-        msgImageWidthSmall: 'Ancho de la imagen de archivo "{name}" debe ser al menos {size} px.',
-        msgImageHeightSmall: 'Altura de la imagen de archivo "{name}" debe ser al menos {size} px.',
-        msgImageWidthLarge: 'Ancho de la imagen de archivo "{name}" no puede exceder de {size} px.',
-        msgImageHeightLarge: 'Altura de la imagen de archivo "{name}" no puede exceder de {size} px.',
-        dropZoneTitle: 'Arrastre y suelte los archivos aquí &hellip;'
+        msgInvalidFileType: 'Tipo de archivo no válido para "{name}". Sólo archivos "{types}" son permitidos.',
+        msgInvalidFileExtension: 'Extensión de archivo no válido para "{name}". Sólo archivos "{extensions}" son permitidos.',
+        msgValidationError: 'Error al subir archivo',
+        msgLoading: 'Subiendo archivo {index} de {files} &hellip;',
+        msgProgress: 'Subiendo archivo {index} de {files} - {name} - {percent}% completado.',
+        msgSelected: '{n} {files} seleccionado(s)',
+        msgFoldersNotAllowed: 'Arrastre y suelte únicamente archivos. Omitida(s) {n} carpeta(s).',
+        msgImageWidthSmall: 'El ancho de la imagen "{name}" debe ser al menos {size} px.',
+        msgImageHeightSmall: 'La altura de la imagen "{name}" debe ser al menos {size} px.',
+        msgImageWidthLarge: 'El ancho de la imagen "{name}" no puede exceder de {size} px.',
+        msgImageHeightLarge: 'La altura de la imagen "{name}" no puede exceder de {size} px.',
+        dropZoneTitle: 'Arrastre y suelte aquí los archivos &hellip;'
     };
 })(window.jQuery);
