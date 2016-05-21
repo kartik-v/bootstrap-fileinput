73503b49f5e217e2530ee46609b49c9158dd463e
Jussi Aho <jussiaho@users.noreply.github.com>
2016-05-22 01:36:35 +0300

    Fix fileinput_locale_fi.js

--- js/locales/fileinput_locale_fi.js
+++ js/locales/fileinput_locale_fi.js
@@ -11,7 +11,7 @@
 (function ($) {
     "use strict";
 
-    $.fn.fileinputLocales.fi = {
+    $.fn.fileinputLocales['fi'] = {
         fileSingle: 'tiedosto',
         filePlural: 'tiedostot',
         browseLabel: 'Selaa &hellip;',
