d86ed68d2c6a2d96cc9977a35af67482b361c8d7
Kartik Visweswaran <kartikv2@gmail.com>
2017-09-10 00:12:36 +0530

    Update Krajee Explorer Themes

--- themes/explorer/theme.js
+++ themes/explorer/theme.js
@@ -31,7 +31,7 @@
             actions: '{drag}\n' +
             '<div class="file-actions">\n' +
             '    <div class="file-footer-buttons">\n' +
-            '        {upload} {delete} {zoom} {other} ' +
+            '        {upload} {download} {delete} {zoom} {other} ' +
             '    </div>\n' +
             '</div>',
             zoomCache: '<tr style="display:none" class="kv-zoom-cache-theme"><td>' +
