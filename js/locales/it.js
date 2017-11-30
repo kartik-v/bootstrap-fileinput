3c646a9c75b7f9fb0d9fb4a1e1aafd79d35b5ff0
thecocce@gmai.com <thecocce@gmai.com>
2017-11-30 17:14:01 +0100

    Italian translation updated

--- js/locales/it.js
+++ js/locales/it.js
@@ -2,6 +2,7 @@
  * FileInput Italian Translation
  * 
  * Author: Lorenzo Milesi <maxxer@yetopen.it>
+ * Reviewed by : TheCocce <thecocce@gmail.com> on 30/11/2017
  *
  * This file must be loaded after 'fileinput.js'. Patterns in braces '{}', or
  * any HTML markup tags in the messages must not be converted or translated.
@@ -26,11 +27,11 @@
         msgNo: 'No',
         msgNoFilesSelected: '',
         msgCancelled: 'Annullato',
-        msgPlaceholder: 'Select {files}...',
+        msgPlaceholder: 'Seleziona {files}...',
         msgZoomModalHeading: 'Anteprima dettagliata',
-        msgFileRequired: 'Devi selezionare un file per continuare.',
-        msgSizeTooSmall: 'File "{name}" (<b>{size} KB</b>) is too small and must be larger than <b>{minSize} KB</b>.',
-        msgSizeTooLarge: 'Il file "{name}" (<b>{size} KB</b>) eccede la dimensione massima di caricamento di <b>{maxSize} KB</b>.',
+        msgFileRequired: 'Devi selezionare almeno un file per poterlo caricare.',
+        msgSizeTooSmall: 'La dimensione del file "{name}" (<b>{size} KB</b>) è troppo piccola e deve superare i <b>{minSize} KB</b>.',
+        msgSizeTooLarge: 'Il file "{name}" (<b>{size} KB</b>) eccede la dimensione massima di caricamento pari a <b>{maxSize} KB</b>.',
         msgFilesTooLess: 'Devi selezionare almeno <b>{n}</b> {files} da caricare.',
         msgFilesTooMany: 'Il numero di file selezionati per il caricamento <b>({n})</b> eccede il numero massimo di file accettati <b>{m}</b>.',
         msgFileNotFound: 'File "{name}" non trovato!',
@@ -38,7 +39,7 @@
         msgFileNotReadable: 'Il file "{name}" non \xE8 leggibile.',
         msgFilePreviewAborted: 'Generazione anteprima per "{name}" annullata.',
         msgFilePreviewError: 'Errore durante la lettura del file "{name}".',
-        msgInvalidFileName: 'Invalid or unsupported characters in file name "{name}".',
+        msgInvalidFileName: 'Sono presenti caratteri non supportati o non validi nel nome del file "{name}".',
         msgInvalidFileType: 'Tipo non valido per il file "{name}". Sono ammessi solo file di tipo "{types}".',
         msgInvalidFileExtension: 'Estensione non valida per il file "{name}". Sono ammessi solo file con estensione "{extensions}".',
         msgFileTypes: {
@@ -51,52 +52,52 @@
             'pdf': 'PDF',
             'object': 'object'
         },
-        msgUploadAborted: 'Il caricamento del file è stata interrotta',
-        msgUploadThreshold: 'Processo...',
-        msgUploadBegin: 'Inizializzazione...',
-        msgUploadEnd: 'Fatto',
-        msgUploadEmpty: 'Dati non disponibili',
+        msgUploadAborted: 'Il caricamento del file è stato interrotto',
+        msgUploadThreshold: 'Processo in corso...',
+        msgUploadBegin: 'Inizializzazione in corso...',
+        msgUploadEnd: 'Caricamento completato',
+        msgUploadEmpty: 'Nessun dato valido per il caricamento.',
         msgUploadError: 'Errore',
         msgValidationError: 'Errore di convalida',
         msgLoading: 'Caricamento file {index} di {files}&hellip;',
         msgProgress: 'Caricamento file {index} di {files} - {name} - {percent}% completato.',
         msgSelected: '{n} {files} selezionati',
         msgFoldersNotAllowed: 'Trascina solo file! Ignorata/e {n} cartella/e.',
-        msgImageWidthSmall: 'Larghezza di file immagine "{name}" deve essere di almeno {size} px.',
-        msgImageHeightSmall: 'Altezza di file immagine "{name}" deve essere di almeno {size} px.',
-        msgImageWidthLarge: 'Larghezza di file immagine "{name}" non può superare {size} px.',
-        msgImageHeightLarge: 'Altezza di file immagine "{name}" non può superare {size} px.',
+        msgImageWidthSmall: 'La larghezza del file immagine "{name}" deve essere di almeno di {size} px.',
+        msgImageHeightSmall: 'L\' altezza del file immagine "{name}" deve essere di almeno {size} px.',
+        msgImageWidthLarge: 'La larghezza del file immagine "{name}" non può superare {size} px.',
+        msgImageHeightLarge: 'L\' altezza del file immagine "{name}" non può superare {size} px.',
         msgImageResizeError: "Impossibile ottenere le dimensioni dell'immagine per ridimensionare.",
         msgImageResizeException: "Errore durante il ridimensionamento dell'immagine.<pre>{errors}</pre>",
-        msgAjaxError: 'Something went wrong with the {operation} operation. Please try again later!',
-        msgAjaxProgressError: '{operation} failed',
+        msgAjaxError: 'Qualcosa non è andato a buon fine con l\'operazione {operation} . Per favore riporva più tardi!',
+        msgAjaxProgressError: '{operation} fallita',
         ajaxOperations: {
-            deleteThumb: 'file delete',
-            uploadThumb: 'file upload',
-            uploadBatch: 'batch file upload',
-            uploadExtra: 'form data upload'
+            deleteThumb: 'cancella file',
+            uploadThumb: 'carica file',
+            uploadBatch: 'carica file multipli',
+            uploadExtra: 'form caricamento dati'
         },
         dropZoneTitle: 'Trascina i file qui&hellip;',
-        dropZoneClickTitle: '<br>(or click to select {files})',
+        dropZoneClickTitle: '<br>(oppure clicca qui per selezionare i {files})',
         fileActionSettings: {
             removeTitle: 'Rimuovere il file',
             uploadTitle: 'Caricare un file',
-            uploadRetryTitle: 'Retry upload',
+            uploadRetryTitle: 'Riprova caricamento',
             downloadTitle: 'Download file',
             zoomTitle: 'Guarda i dettagli',
-            dragTitle: 'Move / Rearrange',
+            dragTitle: 'Muovi / arrangia files',
             indicatorNewTitle: 'Non ancora caricato',
             indicatorSuccessTitle: 'Caricati',
-            indicatorErrorTitle: 'Carica Errore',
+            indicatorErrorTitle: 'Errore caricamento',
             indicatorLoadingTitle: 'Caricamento ...'
         },
         previewZoomButtonTitles: {
-            prev: 'Vedi file precedente',
-            next: 'Vedi file seguente',
-            toggleheader: 'Aziona header',
-            fullscreen: 'Aziona full screen',
-            borderless: 'Abilita modalità senza bordi',
-            close: 'Chiudi'
+            prev: 'Visualizza file precedente',
+            next: 'Visualizza file successivo',
+            toggleheader: 'Attiva / disattiva intestazione',
+            fullscreen: 'Attiva / disattiva modalità schermo intero',
+            borderless: 'Atttiva / disattiva modalità senza bordi',
+            close: 'Chiudi dettaglio anteprima'
         }
     };
 })(window.jQuery);
