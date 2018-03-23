e44f771e4a7d5836add79d52a016cf6a500c37c1
Kartik Visweswaran <kartikv2@gmail.com>
2018-03-23 15:42:29 +0530

    Update Font Awesome 5 Theme fixes #1197

--- themes/fas/theme.js
+++ themes/fas/theme.js
@@ -32,10 +32,10 @@
         previewZoomButtonIcons: {
             prev: '<i class="fas fa-caret-left fa-lg"></i>',
             next: '<i class="fas fa-caret-right fa-lg"></i>',
-            toggleheader: '<i class="fas fa-arrows-alt-v"></i>',
-            fullscreen: '<i class="fas fa-expand-arrows-alt"></i>',
-            borderless: '<i class="fas fa-external-link-alt"></i>',
-            close: '<i class="fas fa-times"></i>'
+            toggleheader: '<i class="fas fa-fw fa-arrows-v"></i>',
+            fullscreen: '<i class="fas fa-fw fa-arrows-alt"></i>',
+            borderless: '<i class="fas fa-fw fa-external-link"></i>',
+            close: '<i class="fas fa-fw fa-times"></i>'
         },
         previewFileIcon: '<i class="fas fa-file"></i>',
         browseIcon: '<i class="fas fa-folder-open"></i>',
