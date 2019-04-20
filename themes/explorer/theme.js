91fe189f47ef3f73422b67b7d833b5f5ceb6f846
Kartik Visweswaran <kartikv2@gmail.com>
2019-04-20 10:47:51 +0530

    Prepare for release v5.0.3

--- themes/explorer/theme.js
+++ themes/explorer/theme.js
@@ -1,5 +1,5 @@
 /*!
- * bootstrap-fileinput v5.0.2
+ * bootstrap-fileinput v5.0.3
  * http://plugins.krajee.com/file-input
  *
  * Krajee Explorer theme configuration for bootstrap-fileinput. Load this theme file after loading `fileinput.js`.
--- themes/fa/theme.min.js
+++ themes/fa/theme.min.js
@@ -1,5 +1,5 @@
 /*!
- * bootstrap-fileinput v5.0.2
+ * bootstrap-fileinput v5.0.3
  * http://plugins.krajee.com/file-input
  *
  * Font Awesome icon theme configuration for bootstrap-fileinput. Requires font awesome assets to be loaded.
