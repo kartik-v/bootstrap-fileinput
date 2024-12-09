b7be3a15634597353d9b59cb7d40be4c6dcb378d
miketsoft <miketsoft@gmail.com>
2024-12-09 14:59:37 +0200

    fix: try to fallback l10n to language

--- js/fileinput.js
+++ js/fileinput.js
@@ -7190,8 +7190,15 @@
         if (theme) {
           t = $.fn.fileinputThemes[theme] || {};
         }
-        if (lang !== "en" && !$h.isEmpty($.fn.fileinputLocales[lang])) {
-          l = $.fn.fileinputLocales[lang] || {};
+        if (lang !== "en") {
+          if (!$h.isEmpty($.fn.fileinputLocales[lang])) {
+            l = $.fn.fileinputLocales[lang];
+          } else {
+            var langParts = lang.split('-', 2);
+            if (langParts.length > 1 && !$h.isEmpty($.fn.fileinputLocales[langParts[0]])) {
+              l = $.fn.fileinputLocales[langParts[0]];
+            }
+          }
         }
         opt = $.extend(true, {}, $.fn.fileinput.defaults, t, $.fn.fileinputLocales.en, l, optObj, self.data());
         data = new FileInput(this, opt);
